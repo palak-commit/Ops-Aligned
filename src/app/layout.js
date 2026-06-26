2b3ef5d1560b65fb65b820a0f23a1b51243bea78
palak-commit <palakpatel00132@gmail.com>
2026-06-26 15:22:32 +0530

    feat: add Google AdSense verification script to document head

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,6 +32,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google AdSense Verification */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3720190862522195" crossOrigin="anonymous"></script>
+
+        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
         <script
           dangerouslySetInnerHTML={{
